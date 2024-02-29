ed2939a616e8991d50a4addeb53688576834b482
Lyssieth <lys@lys.ee>
2024-02-29 15:17:03 +0200

    Fix the default.nix again

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
   import (
     builtins.fetchTarball {
       url = "https://github.com/edolstra/flake-compat/archive/master.tar.gz";
-      sha256 = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
+      sha256 = "sha256:0m9grvfsbwmvgwaxvdzv6cmyvjnlww004gfxjvcl806ndqaxzy4j";
     }
   ) {
     src = ./.;
