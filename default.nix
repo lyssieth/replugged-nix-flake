9371cab68d514164f8b024d7b491d6684e144449
Lyssieth <lys@lys.ee>
2024-02-29 15:28:05 +0200

    adjust default.nix

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 (
   import (
     builtins.fetchTarball {
-      url = "https://github.com/edolstra/flake-compat/archive/master.tar.gz";
+      url = "https://github.com/edolstra/flake-compat/archive/0f9255e01c2351cc7d116c072cb317785dd33b33.tar.gz";
       sha256 = "sha256:0m9grvfsbwmvgwaxvdzv6cmyvjnlww004gfxjvcl806ndqaxzy4j";
     }
   ) {
